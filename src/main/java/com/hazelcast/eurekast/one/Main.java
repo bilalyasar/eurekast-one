35da4ea67aa8f9236edf8073433c02e317cc143b
bilalyasar <bilalyasar45@gmail.com>
2016-12-08 03:40:49 +0300

    wip

---
+++ src/main/java/com/hazelcast/eurekast/one/Main.java
@@ -0,0 +1,12 @@
+package com.hazelcast.eurekast.one;
+
+import com.hazelcast.core.Hazelcast;
+
+/**
+ * Created by bilal on 12/8/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hazelcast.newHazelcastInstance();
+    }
+}
